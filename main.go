69951438eeb9654282f73e4907d66966b0316fc6
Chris <christiano.amora@gmail.com>
2020-12-08 19:05:10 -0300

    add sqlx

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
+
+var schema = `
+CREATE TABLE IF NOT EXISTS music (
+	id SERIAL NOT NULL PRIMARY KEY,
+	title text,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+`
+
+type Music struct {
+	ID        int64     `db:"id"`
+	Title     string    `db:"title"`
+	UpdatedAt time.Time `db:"updated_at"`
+	CreatedAt time.Time `db:"created_at"`
+}
 
 func getPosts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -31,14 +48,21 @@ func init() {
 	connectionString :=
 		fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", dbUser, dbPass, dbName)
 
-	dbConn, err := sql.Open("postgres", connectionString)
+	db, err := sqlx.Connect("postgres", connectionString)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = dbConn.Ping()
+	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
 	}
+	db.MustExec(schema)
+	musics := []Music{}
+	err = db.Select(&musics, "SELECT * FROM music")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(musics)
 }
 
 func main() {
